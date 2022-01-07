156fd73f8f141b723376054f757c202f9f6820e9
Federico Barbieri <federico.barbieri99@gmail.com>
2022-01-07 23:33:21 +0100

    respawn logic

--- enemy.py
+++ enemy.py
@@ -14,7 +14,7 @@ class Enemy(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(walking_sheet, (117, 96))
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        self.rect.x = self.screen_rect.left if self.seed == 0 else self.screen_rect.right
+        self.rect.x = self.screen_rect.left - self.rect.width if self.seed == 0 else self.screen_rect.right
         self.rect.bottom = self.screen_rect.bottom
         self.vel = 10 if self.seed == 0 else -10
         self.walking_animation = [SpriteSheet(walking_sheet).get_image(x, 0, 117, 96) for x in range(0, 468, 117)]
--- main.py
+++ main.py
@@ -10,8 +10,6 @@ screen_size = background.get_rect().size
 screen = pygame.display.set_mode(screen_size, vsync=1)
 background = background.convert()
 font = pygame.font.Font(None, 60)
-
-
 enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
@@ -19,31 +17,35 @@ player = Player(screen)
 player_group.add(player)
 
 clock = pygame.time.Clock()
-gameover = False
+score = 0
 
 
-def update_screen():
-
-    screen.blit(background, [0, 0])
+def update_entities():
+    global score
     player_group.update()
     enemy_group.update()
     bullet_group.update()
-    pygame.sprite.groupcollide(enemy_group, bullet_group, True, True)
+    score += len(pygame.sprite.groupcollide(enemy_group, bullet_group, True, True))
     if pygame.sprite.spritecollideany(player, enemy_group):
         enemy_group.empty()
         bullet_group.empty()
+        player.alive = False
         player.kill()
-        return True
-    print('b')
+
+
+def draw_screen():
+    global score
+
+    screen.blit(background, [0, 0])
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+    screen.blit(score_text, score_text.get_rect())
     player_group.draw(screen)
     enemy_group.draw(screen)
     bullet_group.draw(screen)
-
     pygame.display.flip()
-    return False
 
 
-def event_handler(gameover):
+def event_handler():
     for event in pygame.event.get():
 
         if event.type == pygame.KEYDOWN:
@@ -51,37 +53,51 @@ def event_handler(gameover):
                 player.orientation = 'Left'
             if event.key == pygame.K_RIGHT:
                 player.orientation = 'Right'
-            if event.key == pygame.K_SPACE and not gameover:
+            if event.key == pygame.K_SPACE and player.alive:
                 bullet_group.add(Bullet(screen, player))
                 enemy_group.add(Enemy(screen))
-            if event.key == pygame.K_r and gameover:
-                return False
+            if event.key == pygame.K_r and not player.alive:
+                main()
 
         if event.type == pygame.QUIT:
             sys.exit()
 
 
 def new_game():
+    global score
     screen.fill((0, 0, 0))
-    respawn_text = font.render("Press R to Respawn", False, (255, 255, 255))
-    rect = respawn_text.get_rect()
-    rect.center = screen.get_rect().center
-    screen.blit(respawn_text, rect)
+    score_text = font.render(f"Last score: {score}", True, (255, 255, 255))
+    respawn_text = font.render("Press R to Respawn", True, (255, 255, 255))
+    respawn_rect = respawn_text.get_rect()
+    respawn_rect.centerx = screen.get_rect().centerx
+    respawn_rect.centery = screen.get_rect().centery - (screen.get_rect().centery / 6)
+    score_rect = score_text.get_rect()
+    score_rect.centerx = screen.get_rect().centerx
+    score_rect.centery = screen.get_rect().centery + (screen.get_rect().centery / 6)
+    screen.blit(respawn_text, respawn_rect)
+    screen.blit(score_text, score_rect)
+    pygame.display.flip()
+
+
+def reset():
+    global score
+    enemy_group.empty()
+    bullet_group.empty()
+    player.alive = True
+    player_group.add(player)
+    score = 0
 
 
 def main():
-
+    reset()
     while True:
-
         clock.tick(60)
-        global gameover
-        if not gameover:
-            event_handler(gameover)
-            gameover = update_screen()
+        event_handler()
+        update_entities()
+        if player.alive:
+            draw_screen()
         else:
             new_game()
-            gameover = event_handler(gameover)
-
 
 
 main()
